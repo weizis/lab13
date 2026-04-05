9b302a05daaf58407833de3024ca0662bca0f8f7
weizis <nastyamik2019@mail.ru>
2026-04-05 22:20:53 +0300

    Задание 6-7: getSalary считает сумму зарплат, age private, setAge изменяет возраст

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 class Worker {
     public $name;
-    public $age;
+    private $age;
     public $salary;
     
     public function __construct($name, $age, $salary) {
@@ -12,30 +12,42 @@ class Worker {
     public function getName(){
 	return $this->name;
     }
+    public function setAge($newAge){
+	$this->age=$newAge;
+    }
     public function getAge(){
 	return $this->age;
     }
-    public function getSalary(){
-	return $this->salary;
+    public function getSalary($workers){
+	  $total=0;
+	  foreach ($workers as $worker){
+		$total+=$worker->salary;
+	  }
+	  return $total;
     }
-
 }
+
 
 $worker1 = new Worker("Иван Петров", 25, 50000);
 $worker2 = new Worker("Анна Сидорова", 30, 60000);
+$workers = [$worker1, $worker2];
+
 echo "Работник 1:<br>";
 echo "  getName(): " . $worker1->getName() . "<br>";
 echo "  getAge(): " . $worker1->getAge() . "<br>";
-echo "  getSalary(): " . $worker1->getSalary() . " руб.<br>";
+echo "  salary: " . $worker1->salary . " руб.<br>";
 
 echo "Работник 2:<br>";
 echo "  getName(): " . $worker2->getName() . "<br>";
 echo "  getAge(): " . $worker2->getAge() . "<br>";
-echo "  getSalary(): " . $worker2->getSalary() . " руб.<br>";
+echo "  salary: " . $worker2->salary . " руб.<br>";
 
-$sumSalary = $worker1->salary + $worker2->salary;
-$sumAge = $worker1->age + $worker2->age;
-echo "Сумма зарплат: " . $sumSalary . " руб.<br>";
+$totalSalary = $worker1->getSalary($workers);
+$sumAge = $worker1->getAge() + $worker2->getAge();
+echo "Сумма зарплат через getSalary(): " . $totalSalary . " руб.<br>";
 echo "Сумма возрастов: " . $sumAge . " лет.<br>";
 
+echo "До изменения: возраст worker1 = " . $worker1->getAge() . "<br>";
+$worker1->setAge(35);
+echo "После setAge(35): возраст worker1 = " . $worker1->getAge() . "<br>";
 ?>
