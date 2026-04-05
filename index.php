83117b3c86b3a7e2726c53ed7fe6a5011ca9e7bc
weizis <nastyamik2019@mail.ru>
2026-04-05 23:44:29 +0300

    Задание 9: добавлен метод checkAge

--- index.php
+++ index.php
@@ -30,6 +30,13 @@ class Worker {
 	  }
 	  return $total;
     }
+    public function checkAge(){
+	if ($this->age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
 }
 
 
@@ -37,25 +44,19 @@ $worker1 = new Worker("Иван Петров", 25, 50000);
 $worker2 = new Worker("Анна Сидорова", 30, 60000);
 $workers = [$worker1, $worker2];
 
-echo "Работник 1:<br>";
-echo "  getName(): " . $worker1->getName() . "<br>";
-echo "  getAge(): " . $worker1->getAge() . "<br>";
-echo "  salary: " . $worker1->salary . " руб.<br>";
-
-echo "Работник 2:<br>";
-echo "  getName(): " . $worker2->getName() . "<br>";
-echo "  getAge(): " . $worker2->getAge() . "<br>";
-echo "  salary: " . $worker2->salary . " руб.<br>";
+echo "1. " . $worker1->getName() . ", возраст: " . $worker1->getAge() . ", зарплата: " . $worker1->salary . " руб.<br>";
+echo "2. " . $worker2->getName() . ", возраст: " . $worker2->getAge() . ", зарплата: " . $worker2->salary . " руб.<br>";
 
 $totalSalary = $worker1->getSalary($workers);
 $sumAge = $worker1->getAge() + $worker2->getAge();
 echo "Сумма зарплат через getSalary(): " . $totalSalary . " руб.<br>";
 echo "Сумма возрастов: " . $sumAge . " лет.<br>";
 
-echo "До изменения: возраст worker1 = " . $worker1->getAge() . "<br>";
-$worker1->setAge(35);
-echo "После setAge(35): возраст worker1 = " . $worker1->getAge() . "<br>";
-echo "Меняем возраст worker1:<br>";
+echo "Работник 2 (30 лет): checkAge() = " .
+ ($worker2->checkAge() ? "true (больше 18)" : "false (не больше 18)") . "<br>";
+
+echo "Меняем возраст worker1 на 16:<br>";
 $worker1->setAge(16);
 echo "Возраст остался: " . $worker1->getAge() . "<br>";
+
 ?>
