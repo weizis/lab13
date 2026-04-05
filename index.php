757a4acf123ddd5a0357c423fda959b2e6991eea
weizis <nastyamik2019@mail.ru>
2026-04-05 21:48:23 +0300

    Задание 3-5: добавлены методы getName, getAge, getSalary и их вывод

--- index.php
+++ index.php
@@ -9,14 +9,29 @@ class Worker {
         $this->age = $age;
         $this->salary = $salary;
     }
+    public function getName(){
+	return $this->name;
+    }
+    public function getAge(){
+	return $this->age;
+    }
+    public function getSalary(){
+	return $this->salary;
+    }
+
 }
 
 $worker1 = new Worker("Иван Петров", 25, 50000);
 $worker2 = new Worker("Анна Сидорова", 30, 60000);
-echo "Работник 1: " . $worker1->name . ", " . $worker1->age . " лет, " .
- $worker1->salary . " руб.<br>";
-echo "Работник 2: " . $worker2->name . ", " . $worker2->age . " лет, " .
- $worker2->salary . " руб.<br>";
+echo "Работник 1:<br>";
+echo "  getName(): " . $worker1->getName() . "<br>";
+echo "  getAge(): " . $worker1->getAge() . "<br>";
+echo "  getSalary(): " . $worker1->getSalary() . " руб.<br>";
+
+echo "Работник 2:<br>";
+echo "  getName(): " . $worker2->getName() . "<br>";
+echo "  getAge(): " . $worker2->getAge() . "<br>";
+echo "  getSalary(): " . $worker2->getSalary() . " руб.<br>";
 
 $sumSalary = $worker1->salary + $worker2->salary;
 $sumAge = $worker1->age + $worker2->age;
