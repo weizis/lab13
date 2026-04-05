138df4c03b4b7d729c6026ad7ade7fb5241db793
weizis <nastyammik2019@mail.ru>
2026-04-05 18:39:46 +0300

    Задание 1: создан класс Worker и 2 объекта

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+class Worker {
+    public $name;
+    public $age;
+    public $salary;
+    
+    public function __construct($name, $age, $salary) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
+}
+
+$worker1 = new Worker("Иван Петров", 25, 50000);
+$worker2 = new Worker("Анна Сидорова", 30, 60000);
+echo "Работник 1: " . $worker1->name . ", " . $worker1->age . " лет, " .
+ $worker1->salary . " руб.<br>";
+echo "Работник 2: " . $worker2->name . ", " . $worker2->age . " лет, " .
+ $worker2->salary . " руб.<br>";
+?>
