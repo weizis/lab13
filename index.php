a4943ce2bde463ddf81b7443e074a901fee98bd6
weizis <nastyamik2019@mail.ru>
2026-04-05 21:35:42 +0300

    Задание 2: добавлен вывод суммы зарплат и суммы возрастов

--- index.php
+++ index.php
@@ -17,4 +17,10 @@ echo "Работник 1: " . $worker1->name . ", " . $worker1->age . " лет, 
  $worker1->salary . " руб.<br>";
 echo "Работник 2: " . $worker2->name . ", " . $worker2->age . " лет, " .
  $worker2->salary . " руб.<br>";
+
+$sumSalary = $worker1->salary + $worker2->salary;
+$sumAge = $worker1->age + $worker2->age;
+echo "Сумма зарплат: " . $sumSalary . " руб.<br>";
+echo "Сумма возрастов: " . $sumAge . " лет.<br>";
+
 ?>
